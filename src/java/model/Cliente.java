f3a6329423af368bf33651af422674a0c4eb50b4
Alfonso Ríos <malforime@gmail.com>
2016-04-04 11:48:05 -0500

    Init project 16/04/04

---
+++ src/java/model/Cliente.java
@@ -0,0 +1,115 @@
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 0264ARIOS
+ */
+public class Cliente {
+    private int idCliente;
+    private String nombre;
+    private String apPaterno;
+    private String apMaterno;
+    private Date fechaAlta;
+    private int credito;
+    private int deuda;
+
+    /**
+     * @return the idCliente
+     */
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    /**
+     * @param idCliente the idCliente to set
+     */
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apPaterno
+     */
+    public String getApPaterno() {
+        return apPaterno;
+    }
+
+    /**
+     * @param apPaterno the apPaterno to set
+     */
+    public void setApPaterno(String apPaterno) {
+        this.apPaterno = apPaterno;
+    }
+
+    /**
+     * @return the apMaterno
+     */
+    public String getApMaterno() {
+        return apMaterno;
+    }
+
+    /**
+     * @param apMaterno the apMaterno to set
+     */
+    public void setApMaterno(String apMaterno) {
+        this.apMaterno = apMaterno;
+    }
+
+    /**
+     * @return the fechaAlta
+     */
+    public Date getFechaAlta() {
+        return fechaAlta;
+    }
+
+    /**
+     * @param fechaAlta the fechaAlta to set
+     */
+    public void setFechaAlta(Date fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+
+    /**
+     * @return the credito
+     */
+    public int getCredito() {
+        return credito;
+    }
+
+    /**
+     * @param credito the credito to set
+     */
+    public void setCredito(int credito) {
+        this.credito = credito;
+    }
+
+    /**
+     * @return the deuda
+     */
+    public int getDeuda() {
+        return deuda;
+    }
+
+    /**
+     * @param deuda the deuda to set
+     */
+    public void setDeuda(int deuda) {
+        this.deuda = deuda;
+    }
+}
